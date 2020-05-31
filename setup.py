52e28514d72c7935a12151d8106030272f5fb8db
Bertik23 <bertikxxiii@gmail.com>
2020-05-31 19:46:27 +0200

    second try

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="bdbf", # Replace with your own username
-    version="0.0.2",
+    version="0.0.3",
     author="Bertik23",
     author_email="bertikxxiii@gmail.com",
     description="My discord bot framework",
